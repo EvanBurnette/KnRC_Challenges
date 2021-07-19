f9b71489acfee91cbd664da66943f08a49739300
Evan Burnette <EvanBurnette@users.noreply.github.com>
2021-07-19 06:41:02 -1000

    Filled out pseudocode for fold.c

--- fold.c
+++ fold.c
@@ -6,20 +6,45 @@
 #define MAXLINE 1000
 #define FOLDAT 80
 
-int foldLine ( char line[] );
+void foldLine ( char line[] );
 int getmyline ( char line[] );
 
 int main() {
   //Loop through all lines in a file while not done
-    //Get each line and put it in 'myline' variable
+    //Get each line and put it in 'myline' variable, set length with return from getmyline function
     //If line length is greater than FOLDLENGTH
       //Send it to the foldLine function
     //Else
       //print the line
 }
 
-int foldLine ( char line[] ){
+void foldLine ( char line[] ){
   //Iterate through the beginning of the line, collecting # of tabs
-  //Add tab constant * # of tabs to current column
-  //When not 
+  //Add tab constant * # of tabs to currentColumn
+  //When char c is not a tab, turn begin line tab counting off
+  //For each character in line
+    //Store index start position
+    //Count each character up to FOLDAT
+    //If character is tab
+      //Add tab const up to next tab stop
+    //Else if character is normal
+      //Increment currentColumn
+    //When currentColumn > 80
+      //while character is not a space or a tab
+        //Decrement index
+        //Read in last character
+      //Print characters from index start up to index
+      //Print newline + (# of tabs + 1) to next line
+      //Set currentColumn to # of tabs*tab constant
+}
+int done = 0;
+
+int getmyline() {
+  char c;
+  int l = 0;
+  while ( ((c = getchar()) != EOF) && c != '/n'){
+    ++l;
+  }
+  if (c = EOF) done = 1;
+  return l;
 }
